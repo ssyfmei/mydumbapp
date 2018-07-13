f46657f1e25a356f31f9c9e8cc6802ef113b326b
meixx115 <meixx115@d.umn.edu>
2018-07-12 22:38:11 -0500

    .

--- src/main/java/com/yifeng/app/StudentControllerServlet.java
+++ src/main/java/com/yifeng/app/StudentControllerServlet.java
@@ -6,11 +6,13 @@ import java.util.List;
 import javax.annotation.Resource;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
+@WebServlet("/StudentControllerServlet")
 public class StudentControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
