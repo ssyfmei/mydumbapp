6428262562bb94c01283e99e2ae6282ed8bdc21f
meixx115 <meixx115@d.umn.edu>
2018-07-13 14:20:54 -0500

    .

--- src/main/java/com/yifeng/app/StudentControllerServlet.java
+++ src/main/java/com/yifeng/app/StudentControllerServlet.java
@@ -43,7 +43,7 @@ public class StudentControllerServlet extends HttpServlet {
 	private void listStudents(HttpServletRequest request, HttpServletResponse response) throws Exception{
 		List<Student> students = studentDbUtil.getStudents();
 		request.setAttribute("STUDENT_LIST", students);
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/list-students.jsp");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/list-student.jsp");
 		dispatcher.forward(request, response);
 	}
 	
